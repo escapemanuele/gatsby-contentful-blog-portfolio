38f3c24c7fdafcc67cd2fcf0875c9ef1a9bbd8a3
escapemanuele <escapemanuele@gmail.com>
2020-06-17 16:36:24 +0200

    defer quicklink and websitecarbon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from 'react-helmet'
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import { ThemeProvider } from "styled-components"
@@ -15,21 +16,11 @@ const Layout = ({ children }) => {
         <Footer />
       </ThemeProvider>
       <Script
-        src="https://cdnjs.cloudflare.com/ajax/libs/quicklink/2.0.0-alpha/quicklink.umd.js"
-        defer />
-      <Script
-          innerHTMLCode={`
-            window.addEventListener('load', () => quicklink.listen());
-          `}
-        />
-      <Script src="https://unpkg.com/website-carbon-badges@^1/b.min.js" defer />
-      <Script
         innerHTMLCode={`
             document.querySelector('.logo-btn').addEventListener('click', function (){
               document.querySelector('.nav-links').classList.toggle("show-nav");
             })
-          `}
-      />
+          `} />
     </main>
   )
 }
---
+++ src/html.js
@@ -0,0 +1,61 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Script from './components/Script'
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+        <Script
+            innerHTMLCode={`
+                function appendScript(url) {
+                    var element = document.createElement("script");
+                    element.src = url;
+                    document.body.appendChild(element);
+                }
+
+                function downloadJSAtOnload() {
+                    appendScript("https://cdnjs.cloudflare.com/ajax/libs/quicklink/2.0.0-alpha/quicklink.umd.js")
+                    appendScript("https://unpkg.com/website-carbon-badges@^1/b.min.js")
+                }
+                if (window.addEventListener)
+                    window.addEventListener("load", downloadJSAtOnload, false);
+                else if (window.attachEvent)
+                    window.attachEvent("onload", downloadJSAtOnload);
+                else window.onload = downloadJSAtOnload;
+
+                window.addEventListener('load', () => quicklink.listen());
+            `}
+        />
+        <script>
+            
+        </script>
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
